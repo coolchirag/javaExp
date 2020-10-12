c6890f5e6c5db52e44f0d5de7d82b7333832301a
chiragj <chirag.j@ezdi.us>
2020-10-12 13:05:53 +0530

    initial commit

---
+++ DemoProject/src/com/chirag/ClassDemo.java
@@ -0,0 +1,29 @@
+package com.chirag;
+
+public class ClassDemo {
+
+	
+
+	int eclipse;
+
+	int git;
+	
+	int eclipse2;
+	int git2;
+
+	int intalij;
+	
+	int git3;
+	
+	int git4;
+	
+
+	int eclipse3;
+	
+	int git5;
+	
+	int eclipse4;
+	
+	int git6;
+
+}
---
+++ DemoProject/src/com/chirag/ClassDemo2.java
@@ -0,0 +1,17 @@
+package com.chirag;
+
+public class ClassDemo2 {
+
+	int git;
+	
+	int git2;
+	
+	int git3;
+	
+	int eclipse1;
+	
+	int git5;
+	
+	int git 6;
+
+}
