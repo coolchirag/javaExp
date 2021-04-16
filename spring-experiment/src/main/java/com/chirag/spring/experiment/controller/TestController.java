9ae025d866f1348fd9ea4f24675fa5aabd15f1b3
chiragj <chirag.j@ezdi.us>
2021-04-16 15:29:47 +0530

    add large file handling

--- spring-experiment/src/main/java/com/chirag/spring/experiment/controller/TestController.java
+++ spring-experiment/src/main/java/com/chirag/spring/experiment/controller/TestController.java
@@ -1,25 +1,20 @@
 package com.chirag.spring.experiment.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.chirag.spring.experiment.dto.TestRquest;
+
 @RestController
 public class TestController {
 
-	@PostMapping("/")
-	public String loadFile(@RequestParam MultipartFile[] files) {
-		for(MultipartFile file : files) {
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println(file.getOriginalFilename());
-		}
-		return "Done";
-		
+	@PostMapping("/test")
+	public String loadData(@RequestBody(required = false) List<TestRquest> data) {
+		return "hi";
 	}
 }
