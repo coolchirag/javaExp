cfde6759b4ba987382ade3596c74750fe275c548
chirag <chirag@raapid.ai>
2023-03-02 15:21:01 +0530

    add enum

--- DemoProject/src/com/chirag/enumdemo/EnumDemoMain.java
+++ DemoProject/src/com/chirag/enumdemo/EnumDemoMain.java
@@ -5,5 +5,7 @@ public class EnumDemoMain {
 	public static void main(String[] args) {
 		System.out.println(TestEnum.valueOf("TEST1"));
 		System.out.println(TestEnum.valueOf("test2"));
+		TestEnum te = null;
+		System.out.println(TestEnum.TEST1.equals(te));
 	}
 }
