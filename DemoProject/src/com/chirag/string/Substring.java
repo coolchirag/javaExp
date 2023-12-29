7b38beced955baf4c5575a71134855681efd4bff
Chirag Jivani <chirag@raapid.ai>
2023-12-30 02:07:22 +0530

    substring experiment

---
+++ DemoProject/src/com/chirag/string/Substring.java
@@ -0,0 +1,9 @@
+package com.chirag.string;
+
+public class Substring {
+public static void main(String[] args) {
+	String path = " KPMG/Project1/300/document/Carol__Danvers_1703393533365.pdf";
+	path=path.substring(0, path.length()-4);
+	System.out.println(path);
+}
+}
