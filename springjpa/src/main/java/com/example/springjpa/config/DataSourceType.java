1db3011805637c82cd3482037fef47b704007702
ChiragJivaniRaapid <chirag@raapidinc.com>
2026-05-16 19:07:14 +0530

    read-replica db integration

---
+++ springjpa/src/main/java/com/example/springjpa/config/DataSourceType.java
@@ -0,0 +1,6 @@
+package com.example.springjpa.config;
+
+public enum DataSourceType {
+	WRITE,
+	READ
+}
--- springjpa/src/main/java/com/example/springjpa/config/DatabaseConfig.java
+++ springjpa/src/main/java/com/example/springjpa/config/DatabaseConfig.java
@@ -1,33 +1,92 @@
 package com.example.springjpa.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseDataSource;
+import org.springframework.boot.autoconfigure.transaction.TransactionManagerCustomizers;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionManager;
 
 @Configuration
 public class DatabaseConfig {
 	
 	//private static final Logger LOG = LoggerFactory.getLogger(DatabaseConfig.class);
+
+	@Value("${app.datasource.write.url}")
+	private String writeUrl;
+
+	@Value("${app.datasource.write.username}")
+	private String writeUsername;
+
+	@Value("${app.datasource.write.password}")
+	private String writePassword;
+
+	@Value("${app.datasource.read.url}")
+	private String readUrl;
+
+	@Value("${app.datasource.read.username}")
+	private String readUsername;
+
+	@Value("${app.datasource.read.password}")
+	private String readPassword;
 	
-	@Bean
+	//@Bean("testq")
+	public PlatformTransactionManager transactionManager(
+			ObjectProvider<TransactionManagerCustomizers> transactionManagerCustomizers) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManagerCustomizers.ifAvailable((customizers) -> customizers.customize(transactionManager));
+		return transactionManager;
+	}
+	
+	@Bean("writeDataSource")
+	//@Bean
+	//@Primary
+	public DataSource writeDataSource() {
+		return generateDataSource(writeUrl, writeUsername, writePassword, false);
+	}
+
+	@Bean("readOnlyDataSource")
+	public DataSource readOnlyDataSource() {
+		return generateDataSource(readUrl, readUsername, readPassword, false);
+	}
+
 	@Primary
-	public DataSource getDatasource() {
-		//return generateDataSource("test_db@int-coding-platform-document-pipeline", "password", false);
-		return generateDataSource("root", "Chirag@2025", false);
+	@Bean("dataSource")
+	public DataSource dataSource(
+			@Qualifier("writeDataSource") DataSource writeDataSource,
+			@Qualifier("readOnlyDataSource") DataSource readOnlyDataSource) {
+		Map<Object, Object> targetDataSources = new HashMap<>();
+		targetDataSources.put(DataSourceType.WRITE, writeDataSource);
+		targetDataSources.put(DataSourceType.READ, readOnlyDataSource);
+
+		TransactionRoutingDataSource routingDataSource = new TransactionRoutingDataSource();
+		routingDataSource.setTargetDataSources(targetDataSources);
+		routingDataSource.setDefaultTargetDataSource(writeDataSource);
+		routingDataSource.afterPropertiesSet();
+		return new LazyConnectionDataSourceProxy(routingDataSource);
+		//return routingDataSource;
 	}
 	
 	@LiquibaseDataSource
-	@Bean
-	public DataSource getLiquibasedDatasource() {
-		//return generateDataSource("test_db@int-coding-platform-document-pipeline", "password", true);
-		return generateDataSource("root", "Chirag@2025", true);
+	@Bean("liquibaseDataSource")
+	public DataSource liquibaseDataSource(@Qualifier("writeDataSource") DataSource writeDataSource) {
+		return writeDataSource;
 	}
 	
-	private DataSource generateDataSource(String username, String password, boolean isLbDataSource) {
+	private DataSource generateDataSource(String url, String username, String password, boolean isLbDataSource) {
 
 		/*
 		 * String importCert = " -import "+ " -alias mysqlServerCACert "+ " -file " +
@@ -51,10 +110,10 @@ public class DatabaseConfig {
 		 * "/home/chiragjivani/git/javaExp/springjpa/truststore");
 		 * System.setProperty("javax.net.ssl.trustStorePassword","password");
 		 */
-		DataSourceBuilder dsBuilder = DataSourceBuilder.create();
+		DataSourceBuilder<?> dsBuilder = DataSourceBuilder.create();
 		dsBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
 		//dsBuilder.url("jdbc:mysql://int-coding-platform-document-pipeline.mysql.database.usgovcloudapi.net/test_db?useSSL=true&serverTimezone=UTC");
-		dsBuilder.url("jdbc:mysql://localhost:3306/test_db");
+		dsBuilder.url(url);
 		dsBuilder.username(username);
 		dsBuilder.password(password);
 		DataSource dataSource = dsBuilder.build();
@@ -72,7 +131,8 @@ public class DatabaseConfig {
 				apacheDataSource.setMaxActive(10);
 				apacheDataSource.setInitialSize(apacheDataSource.getMinIdle());
 		} else {
-			final String errorMsg = "Connection pool property is not set for DataSource of type : "+dataSource.getClass(); 
+			final String errorMsg = "Connection pool property is not set for DataSource of type : "
+					+ (dataSource == null ? "null" : dataSource.getClass());
 			//LOG.warn(errorMsg);
 			throw new RuntimeException(errorMsg);
 		}
